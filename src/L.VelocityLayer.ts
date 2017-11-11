0d9b64bafeadffae66c7f028796e8a1c9e839b9a
Jordan <onza1101@gmail.com>
2017-11-11 21:31:42 +0100

    -Added L.ControlVelocity.ts wich is used to see the wind speed. -Added the corresponding style inside index.html (should have a better implementation) -Added interpolate and bilinearInterpolateVector and also isValue, floorMod to windy.ts -Added initMouseHandler to L.VelocityLayer.ts used to listen mouseEvent and display wind speed value.

--- src/L.VelocityLayer.ts
+++ src/L.VelocityLayer.ts
@@ -3,151 +3,174 @@ import CanvasBound from './canvasBound'
 import MapBound from './mapBound';
 import Layer from "./layer";
 import CanvasLayer from './L.CanvasLayer';
+import VelocityControl from './L.ControlVelocity'
 import * as L from 'leaflet';
 
 
 const L_CanvasLayer = (L.Layer ? L.Layer : L.Class).extend(new CanvasLayer());
-let L_canvasLayer = function () {
-	return new L_CanvasLayer();
+let L_canvasLayer = function() {
+  return new L_CanvasLayer();
+};
+
+const L_ControlVelocity = (L.Control).extend(new VelocityControl);
+let L_controlVelocity = function() {
+  return new L_ControlVelocity();
 };
 
 export default class VelocityLayer {
 
-    private options: any;
-    private _map: any = null;
-	private _canvasLayer: any = null;
-	private _windy: Windy = null;
-	private _context: any = null;
-	private _displayTimeout: number = 0;
-    
-    constructor () {
-        this.options = {
-            displayValues: true,
-            displayOptions: {
-                velocityType: 'Velocity',
-                position: 'bottomleft',
-                emptyString: 'No velocity data'
-            },
-            maxVelocity: 10, // used to align color scale
-            colorScale: null,
-            data: null
-        };
+  private options: any;
+  private _map: any = null;
+  private _canvasLayer: any = null;
+  private _windy: Windy = null;
+  private _context: any = null;
+  private _displayTimeout: number = 0;
+  private _mouseControl: any  = null;
+
+  constructor() {
+    this.options = {
+      displayValues: true,
+      displayOptions: {
+        velocityType: 'Velocity',
+        position: 'bottomleft',
+        emptyString: 'No velocity data',
+				angleConvention: 'bearingCCW',
+				speedUnit: 'm/s'
+      },
+      maxVelocity: 10, // used to align color scale
+      colorScale: null,
+      data: null
+    };
+  }
+
+  initialize(options: any) {
+    L.Util.setOptions(this, options);
+  }
+
+  onAdd(map: any) {
+    // create canvas, add overlay control
+    this._canvasLayer = L_canvasLayer().delegate(this);
+    this._canvasLayer.addTo(map);
+
+    this._map = map;
+  }
+
+  onRemove(map: any) {
+    this._destroyWind();
+  }
+
+  setData(data: any) {
+    this.options.data = data;
+
+    if (this._windy) {
+      this._windy.setData(data);
+      this._clearAndRestart();
     }
 
-	initialize(options: any) {
-		L.Util.setOptions(this, options);
-	}
+    (<any>this).fire('load');
+  }
 
-	onAdd(map: any) {
-		// create canvas, add overlay control
-		this._canvasLayer = L_canvasLayer().delegate(this);
-		this._canvasLayer.addTo(map);
-		this._map = map;
-	}
+  /*------------------------------------ PRIVATE ------------------------------------------*/
 
-	onRemove(map: any) {
-		this._destroyWind();
-	}
+  onDrawLayer(overlay: any, params: any) {
+    var self = this;
 
-	setData(data: any) {
-		this.options.data = data;
+    if (!this._windy) {
+      this._initWindy();
+      return;
+    }
 
-		if (this._windy) {
-			this._windy.setData(data);
-			this._clearAndRestart();
-		}
+    if (!this.options.data) {
+      return;
+    }
 
-		(<any>this).fire('load');
-	}
+    if (this._displayTimeout) clearTimeout(self._displayTimeout);
 
-	/*------------------------------------ PRIVATE ------------------------------------------*/
-
-	onDrawLayer(overlay: any, params: any) {
-		var self = this;
-
-		if (!this._windy) {
-			this._initWindy();
-			return;
-		}
-
-		if (!this.options.data) {
-			return;
-		}
-
-		if (this._displayTimeout) clearTimeout(self._displayTimeout);
-
-		this._displayTimeout = setTimeout(function () {
-			self._startWindy();
-		}, 150); // showing velocity is delayed
-	}
-
-	_startWindy() {
-		var bounds = this._map.getBounds();
-		var size = this._map.getSize();
-
-		// bounds, width, height, extent
-		this._windy.start(
-			new Layer(
-				new MapBound(
-					bounds._northEast.lat,
-					bounds._northEast.lng,
-					bounds._southWest.lat,
-					bounds._southWest.lng
-				),
-				new CanvasBound(0,0,size.x, size.y)
-			)
-			
-		);
-	}
-
-	_initWindy() {
-
-		// windy object, copy options
-		const options = (<any>Object).assign({ canvas: this._canvasLayer._canvas }, this.options);
-		this._windy = new Windy(options);
-
-		// prepare context global var, start drawing
-		this._context = this._canvasLayer._canvas.getContext('2d');
-		this._canvasLayer._canvas.classList.add("velocity-overlay");
-		(<any>this).onDrawLayer();
-
-		this._map.on('dragstart',() => {
-			this._windy.stop();
-		});
-
-		this._map.on('dragend', () => {
-			this._clearAndRestart();
-		});
-
-		this._map.on('zoomstart', () => {
-			this._windy.stop();
-		});
-
-		this._map.on('zoomend', () => {
-			this._clearAndRestart();
-		});
-		this._map.on('resize', () => {
-			this._clearWind();
-		});
-	}
+    this._displayTimeout = setTimeout(function() {
+      self._startWindy();
+    }, 150); // showing velocity is delayed
+  }
 
 
 
-	_clearAndRestart(){
-		if (this._context) this._context.clearRect(0, 0, 3000, 3000);
-		if (this._windy) this._startWindy();
-	}
+  _startWindy() {
+    var bounds = this._map.getBounds();
+    var size = this._map.getSize();
 
-	_clearWind() {
-		if (this._windy) this._windy.stop();
-		if (this._context) this._context.clearRect(0, 0, 3000, 3000);
-	}
+    // bounds, width, height, extent
+    this._windy.start(
+      new Layer(
+        new MapBound(
+          bounds._northEast.lat,
+          bounds._northEast.lng,
+          bounds._southWest.lat,
+          bounds._southWest.lng
+        ),
+        new CanvasBound(0, 0, size.x, size.y)
+      )
 
-	_destroyWind() {
-		if (this._displayTimeout) clearTimeout(this._displayTimeout);
-		if (this._windy) this._windy.stop();
-		if (this._context) this._context.clearRect(0, 0, 3000, 3000);
-		this._windy = null;
-		this._map.removeLayer(this._canvasLayer);
-	}
+    );
+  }
+
+  _initWindy() {
+
+    // windy object, copy options
+    const options = (<any>Object).assign({ canvas: this._canvasLayer._canvas }, this.options);
+    this._windy = new Windy(options);
+
+    // prepare context global var, start drawing
+    this._context = this._canvasLayer._canvas.getContext('2d');
+    this._canvasLayer._canvas.classList.add("velocity-overlay");
+    (<any>this).onDrawLayer();
+
+    this._map.on('dragstart', () => {
+      this._windy.stop();
+    });
+
+    this._map.on('dragend', () => {
+      this._clearAndRestart();
+    });
+
+    this._map.on('zoomstart', () => {
+      this._windy.stop();
+    });
+
+    this._map.on('zoomend', () => {
+      this._clearAndRestart();
+    });
+    this._map.on('resize', () => {
+      this._clearWind();
+    });
+
+    this._initMouseHandler();
+  }
+
+  _initMouseHandler() {
+    if (!this._mouseControl && this.options.displayValues) {
+      var options = this.options.displayOptions || {};
+      options['leafletVelocity'] = this;
+      this._mouseControl = L_controlVelocity().delegate(this);
+      this._mouseControl.setWindy(this._windy);
+      this._mouseControl.setOptions(this.options.displayOptions);
+      this._mouseControl.addTo(this._map);
+    }
+  }
+
+  _clearAndRestart() {
+    if (this._context) this._context.clearRect(0, 0, 3000, 3000);
+    if (this._windy) this._startWindy();
+  }
+
+  _clearWind() {
+    if (this._windy) this._windy.stop();
+    if (this._context) this._context.clearRect(0, 0, 3000, 3000);
+  }
+
+  _destroyWind() {
+    if (this._displayTimeout) clearTimeout(this._displayTimeout);
+    if (this._windy) this._windy.stop();
+    if (this._context) this._context.clearRect(0, 0, 3000, 3000);
+    this._windy = null;
+    this._map.removeLayer(this._canvasLayer);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import MapBound from "./mapBound";
 import Windy from "./windy";
 import CanvasLayer from "./L.CanvasLayer";
 import VelocityLayer from "./L.VelocityLayer";
+import VelocityControl from './L.ControlVelocity'
 
 (<any>window).CanvasBound = CanvasBound;
 (<any>window).MapBound = MapBound;
@@ -13,6 +14,11 @@ const L = (<any>window).L;
 L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend(new CanvasLayer());
 L.canvasLayer = function () {
 	return new L.CanvasLayer();
+};
+
+L.ControlVelocity = (L.Control).extend(new VelocityControl);
+L.controlVelocity = function() {
+  return new L.ControlVelocity();
 };
 
 L.VelocityLayer = (L.Layer ? L.Layer : L.Class).extend(new VelocityLayer());
